a2a68d3c8513f863fb7e263853f3df44719e655b
Jasper <jave.ong4@yahoo.com>
2016-06-20 17:50:55 -0400

    created reviews and authorization

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,34 @@
+class ReviewsController < ApplicationController
+  before_action :load_product
+  before_action :ensure_logged_in, only: [:create, :destroy]
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def create
+    @review = @product.reviews.build(review_params)
+    @review.user = current_user
+
+    if @review.save
+      flash[:notice] = "Review created successfully"
+      redirect_to product_path(@product)
+    else
+      render :product
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:comment, :product_id)
+  end
+
+  def load_product
+    @product = Product.find(params[:product_id])
+  end
+end
